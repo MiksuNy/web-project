4c5f99e7471d64fc0ccb91baff664a8356e04fff
Markus Kannisto <m.kannisto01@gmail.com>
2026-01-23 14:31:31 +0200

    Added global.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CreatePostForm from "./components/CreatePostForm/CreatePostForm"
 
 function App() {
   return (
     <>
-      <div>
-        Start the project
-      </div>
+      <CreatePostForm />
     </>
   )
 }
