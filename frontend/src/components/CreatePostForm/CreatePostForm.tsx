28b2fa7b8add50bf1cb7877c07c9b186206169fe
Markus Kannisto <m.kannisto01@gmail.com>
2026-01-23 15:22:02 +0200

    Update styling

--- frontend/src/components/CreatePostForm/CreatePostForm.tsx
+++ frontend/src/components/CreatePostForm/CreatePostForm.tsx
@@ -1,11 +1,11 @@
 export default function CreatePostForm() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col rounded-2xl shadow-xl m-6 p-6 border border-gray-200 max-w-3xl mx-auto">
       <h1>Create a Post</h1>
       <div className="flex flex-col">
-        <div className="flex flex-row">
-          <button id="create-post-offer-button">🤝 I Can Offer Help</button>
-          <button id="create-post-request-button">🙋 I Need Help</button>
+        <div className="flex flex-row justify-stretch gap-2">
+          <button id="create-post-offer-button" className="button-accent1 w-1/2 h-20">🤝 I Can Offer Help</button>
+          <button id="create-post-request-button" className="button-accent2 w-1/2 h-20">🙋 I Need Help</button>
         </div>
         <select id="create-post-category">
           <option>Transportation</option>
@@ -17,7 +17,7 @@ export default function CreatePostForm() {
           <option>Other</option>
         </select>
         <input type="text" id="create-post-title" placeholder="Title" />
-        <textarea id="create-post-description" placeholder="Description"></textarea>
+        <textarea id="create-post-description" placeholder="Description" className="min-h-24"></textarea>
         <input type="file" id="create-post-thumbnail" placeholder="Upload image" />
       </div>
     </div>
