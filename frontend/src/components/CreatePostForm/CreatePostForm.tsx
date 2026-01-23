181329cf8cf8e4c2d7ddc6a789cfa2dd4bf52e2f
Markus Kannisto <m.kannisto01@gmail.com>
2026-01-23 15:03:07 +0200

    Add tailwind

--- frontend/src/components/CreatePostForm/CreatePostForm.tsx
+++ frontend/src/components/CreatePostForm/CreatePostForm.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
 export default function CreatePostForm() {
   return (
-    <div>
-      create
+    <div className="flex flex-col">
+      <h1>Create a Post</h1>
+      <div className="flex flex-col">
+        <div className="flex flex-row">
+          <button id="create-post-offer-button">🤝 I Can Offer Help</button>
+          <button id="create-post-request-button">🙋 I Need Help</button>
+        </div>
+        <select id="create-post-category">
+          <option>Transportation</option>
+          <option>Food</option>
+          <option>Education</option>
+          <option>Technology</option>
+          <option>Home Repair</option>
+          <option>Companionship</option>
+          <option>Other</option>
+        </select>
+        <input type="text" id="create-post-title" placeholder="Title" />
+        <textarea id="create-post-description" placeholder="Description"></textarea>
+        <input type="file" id="create-post-thumbnail" placeholder="Upload image" />
+      </div>
     </div>
   )
 }
