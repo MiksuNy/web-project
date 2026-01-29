3ff6cc209d610549f72aa5b0ffc33eb58c8c1a16
Minoo-YH <minoo.yagoubi@gmail.com>
2026-01-30 00:10:51 +0200

    Resolve conflicts: use repo version for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import CreatePostForm from "./components/CreatePostForm/CreatePostForm";
 import Login from "./components/Login.jsx";
-import Home from "./components/Home.jsx";
+import Footer from "./components/Footer.jsx";
+
+
 
 function App() {
   return (
     <BrowserRouter>
+      <CreatePostForm />
       <Header />
       <Routes> 
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+
+
+      <Footer/>
+
+
+      
     </BrowserRouter>
   );
 }
