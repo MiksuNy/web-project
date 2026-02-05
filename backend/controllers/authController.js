221fe66bdc7a3a8f00ffd4a017a95bda5f20dcd8
Maria K <makuza.mk@gmail.com>
2026-02-05 12:32:07 +0200

    feat: rename to userModel

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const User = require('../models/userModal');
+const User = require('../models/userModel');
 const {
   isValidEmail,
   isValidPassword,
