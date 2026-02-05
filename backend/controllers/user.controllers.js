9cff866f7b13ab2a4404f23380e37c005dde8e64
MiksuNy <miksunyman1000@gmail.com>
2026-02-05 11:24:09 +0200

    Better error handling for user controller with try-catch

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,24 +1,41 @@
 const User = require('../models/User');
+const mongoose = require('mongoose');
 
 // GET /users/:userId
-const getUserById = (req, res) => {
+const getUserById = async (req, res) => {
     const userId = req.params.userId;
-    const user = User.findById(userId);
-    if (user) {
-        res.json(user);
-    } else {
-        res.status(404).json({ message: "User not found" });
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return res.status(400).json({ message: "Invalid user id" });
+    }
+
+    try {
+        const user = await User.findById(userId);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: "User not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error });
     }
 }
 
 // PUT /users/:userId
-const updateUserById = (req, res) => {
+const updateUserById = async (req, res) => {
     const userId = req.params.userId;
-    const updatedUser = User.findByIdAndUpdate(userId, { ...req.body });
-    if (updatedUser) {
-        res.json(updatedUser);
-    } else {
-        res.status(400).json({ message: "User not found" });
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return res.status(400).json({ message: "Invalid user id" });
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(userId, { ...req.body });
+        if (updatedUser) {
+            res.json(updatedUser);
+        } else {
+            res.status(400).json({ message: "User not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error });
     }
 }
 
