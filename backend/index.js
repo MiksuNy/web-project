f372a2af0885de3e329efd15006f7f4d9e857626
MiksuNy <miksunyman1000@gmail.com>
2026-02-06 14:12:46 +0200

    Rename user controller and router

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const locationsRouter = require('./routes/locationsRouter');
 const authRoutes = require('./routes/authRouter');
 const requestRoutes = require('./routes/requestsRouter');
+const userRoutes = require('./routes/userRouter');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 app.use('/auth', authRoutes);
 app.use('/requests', requestRoutes);
 app.use('/api', locationsRouter);
+app.use('/users', userRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
