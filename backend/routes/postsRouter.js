ce60a29f5ec3b3d03b8df0d8b90f81cebee6b029
Maria K <makuza.mk@gmail.com>
2026-02-10 13:49:59 +0200

    feat: update posts by user ID, without auth

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -4,17 +4,20 @@ const auth = require('../middleware/auth');
 const upload = require('../middleware/storage');
 const validatePost = require('../middleware/validatePost');
 
-const { createPost, getPosts, getUserPosts } = require('../controllers/postController');
+const { createPost, getPosts, getUserPosts, updatePost, deletePost } = require('../controllers/postController');
 
 // GET /posts (all)
 router.get('/', getPosts);
+
+// GET /posts/:id (posts by user ID)
+// later add to auth middleware to check if the user is requesting their own posts
+router.get('/:id', getUserPosts);
 
 router.use(auth);
 
 // POST /posts
 router.post('/', upload.single('image'), validatePost, createPost);
-
-// GET /posts/my (current user only)
-router.get('/userposts', getUserPosts);
+router.put('/:id', upload.single('image'), validatePost, updatePost);
+router.delete('/:id', deletePost);
 
 module.exports = router;
