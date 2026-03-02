8d09cde09ae4750a504cbc1b067ce0dd64679755
Marakusa <m.kannisto01@gmail.com>
2026-03-02 10:29:44 +0200

    Remvoe unused import

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import Hero from "../components/Hero";
-import CreatePostForm from "../components/CreatePostForm/CreatePostForm";
 import SearchBox from "../components/SearchBox/SearchBox";
 import { PostCard } from "@/components/post";
 import { posts } from "@/data/posts";
-
-
-
 
 export default function Home() {
   return (
