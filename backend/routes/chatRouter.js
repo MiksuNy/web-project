d6f7d85fb80771ed627f848340f12ecbf04e3bae
Maria K <makuza.mk@gmail.com>
2026-03-05 11:51:15 +0200

    refactor: update chatRouter with the new routes my-chats

--- backend/routes/chatRouter.js
+++ backend/routes/chatRouter.js
@@ -6,6 +6,8 @@ const {
   getMessages,
   sendMessage,
   getChatInfo,
+  getMyChats,
+  deleteChat
 } = require("../controllers/chatController");
 
 router.use(auth);
@@ -13,13 +15,19 @@ router.use(auth);
 // Create chat
 router.post("/", createChat);
 
+// Get my chats
+router.get("/my-chats", getMyChats);
+
 // Get chat info (participants)
 router.get("/:chatId/info", getChatInfo);
 
 // Get messages
-router.get("/:chatId/messages", getMessages);
+router.get("/:chatId", getMessages);
 
 // Send message
-router.post("/:chatId/messages", sendMessage);
+router.post("/:chatId", sendMessage);
+
+// Delete chat
+router.delete("/:chatId", deleteChat);
 
 module.exports = router;
